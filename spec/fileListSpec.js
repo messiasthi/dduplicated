dc9d1edb2e83e863966679e8057db859ece7090f
Thiago Messias <messiasthi@gmail.com>
2016-11-05 04:55:01 -0200

    feat(Lister): Create the module to list files and directories in path.
    Create the module to list files and directories in path and validate results with unit test.
    
    Lister

--- spec/fileListSpec.js
+++ spec/fileListSpec.js
@@ -12,9 +12,24 @@ install();
  * @return {void}
  */
 
-describe('FileLister', () => {
+describe('Lister', () => {
+  it('should be list all files and directories', () => {
+    const list = Lister.listAll(`${process.env.PWD}/spec/dirTest`);
+    const expected = [
+      'dir1',
+      'dir2',
+      'dir3',
+      'dir4',
+      'test1.txt',
+      'test2.txt',
+      'test3.txt',
+      'test4.txt',
+    ];
+    expect(list).toEqual(expected);
+  });
+
   it('should be list only the files in directory', () => {
-    const fileList = Lister.listFile('spec/dirTest');
+    const fileList = Lister.listFiles(`${process.env.PWD}/spec/dirTest`);
     const expected = [
       'test1.txt',
       'test2.txt',
@@ -24,29 +39,14 @@ describe('FileLister', () => {
     expect(fileList).toEqual(expected);
   });
 
-  // it('should be list only directories', () => {
-  //   const dirList = Lister.listDir('spec/dirTest');
-  //   const expected = [
-  //     'dir1',
-  //     'dir2',
-  //     'dir3',
-  //     'dir4',
-  //   ];
-  //   expect(dirList).toEqual(expected);
-  // });
-  //
-  // it('should be list all files and directories', () => {
-  //   const list = Lister.list('spec/dirTest');
-  //   const expected = [
-  //     'dir1',
-  //     'dir2',
-  //     'dir3',
-  //     'dir4',
-  //     'test1.txt',
-  //     'test2.txt',
-  //     'test3.txt',
-  //     'test4.txt',
-  //   ];
-  //   expect(list).toEqual(expected);
-  // });
+  it('should be list only directories', () => {
+    const dirList = Lister.listDir(`${process.env.PWD}/spec/dirTest`);
+    const expected = [
+      'dir1',
+      'dir2',
+      'dir3',
+      'dir4',
+    ];
+    expect(dirList).toEqual(expected);
+  });
 });
--- src/modules/lister.js
+++ src/modules/lister.js
@@ -1,40 +1,83 @@
 /**
  * The lister module contain the methods to list directories and file.
  */
-const shell = require('shelljs');
 const fs = require('fs');
+const path = require('path');
+
+/**
+ * List all content of directory
+ * @param  {String} dirPath The absolute path of directory
+ * @return {Array|Boolean}  Return the `false` in case of the path is invalid or
+ * a file path. In case of success return the Array with all file names and dir names
+ */
+function getDirContent(dirPath) {
+  let dir = path.normalize(dirPath);
+  let res = false;
+
+  if (path.isAbsolute(dir) && fs.lstatSync(dir).isDirectory()) {
+    // Add the end bar in string on case of not found
+    if (dir.substr(dir.length - 1) !== '/') {
+      dir += '/';
+    }
+    // Retrive the all files and directories in content of directory
+    res = fs.readdirSync(dir);
+  }
+  return res;
+}
+
 
 export default class Lister {
-
   /**
    * List only files presents in `path`
    * @method listFiles
    * @param  {String} path The directory path
    * @return {Array}       List of files presents in directory
    */
-  static listFiles(path) {
-    // console.log(path);
+  static listFiles(dirPath) {
+    const dirContent = getDirContent(dirPath);
     const res = [];
-    fs.readdir(path, (err, files) => {
-      files.forEach((file) => {
-        res.push(file);
+    // Check if `dirContent` is Array
+    if (Array.isArray(dirContent)) {
+      // Interate the Array to check if is directory, in case success,
+      // remove the element from Array
+      dirContent.forEach((item) => {
+        if (fs.lstatSync(path.normalize(`${dirPath}/${item}`)).isFile()) {
+          // Add the file name to `res`
+          res.push(item);
+        }
       });
-    });
-    // console.log(res);
+    }
     return res;
   }
-
-  static listDir(path) {
-    console.log(path);
-    const res = shell.ls(path);
-    console.log(res);
+  /**
+   * List only directories presents in `path`
+   * @method listDir
+   * @param  {String} path The directory path
+   * @return {Array}       List of directories presents in directory
+   */
+  static listDir(dirPath) {
+    const dirContent = getDirContent(dirPath);
+    const res = [];
+    // Check if `dirContent` is Array
+    if (Array.isArray(dirContent)) {
+      // Interate the Array to check if is file, in case success,
+      // remove the element from Array
+      dirContent.forEach((item) => {
+        if (fs.lstatSync(path.normalize(`${dirPath}/${item}`)).isDirectory()) {
+          // Add the directory name to `res`
+          res.push(item);
+        }
+      });
+    }
     return res;
   }
-
-  static list(path) {
-    console.log(path);
-    const res = shell.ls(path);
-    console.log(res);
-    return res;
+  /**
+   * List files and directories presents in `path`
+   * @method listDir
+   * @param  {String} path The directory path
+   * @return {Array}       List of directories and files presents in directory
+   */
+  static listAll(dirPath) {
+    return getDirContent(dirPath);
   }
 }
