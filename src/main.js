5ecee3225b6e265c9d153ccf0e0b14fa354b2bd4
messiasthi <messiasthi@gmail.com>
2018-02-07 01:46:56 -0200

    feat(main): Update main
    Update main and help

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 #! /usr/bin/env node
-// import { Directory } from './src/modules/Directory';
-// import File from './src/modules/File';
+import { getDuplicatesSync, getDuplicates, DuplicateScan } from './modules/DuplicateScan';
+
 // Named export to non command line applications
+export default DuplicateScan;
+export { getDuplicatesSync, getDuplicates };
 
 // Define possible commands
 const commands = {
@@ -9,57 +11,93 @@ const commands = {
   detect: 'detect',
   delete: 'delete',
   link: 'link',
-  help: 'help'
+  help: 'help',
 };
+
+// Initialize vars
+const paths = [];
+let command = commands.detect;
 
 /**
  * Show all help commands
+ * @param {String} arg The command specific to show help only for it
  */
 function help(arg) {
   const helpTopics = {
-    path: 'The path to search duplicated files.',
+    path: '\tThe path to search duplicated files.',
     detect: '[Default]Detect the files duplicated in directory',
     delete: 'Delete any duplicated file, only the first detection remain',
-    link: 'Delete any duplicated file, only the first detection remain but, create a link in each detection to first detection',
-    help: 'Show this help',
+    link: '\tDelete any duplicated file, only the first detection remain but, create a link in each detection to first detection',
+    help: '\tShow this help',
   };
 
   console.log('Commands:');
   console.log('dduplicated <command> <path> [<path>');
   if (arg && arg in helpTopics) {
-    console.log(`    ${arg}\t\t${helpTopics[arg]}`);
+    console.log(`\t--${arg}\t${helpTopics[arg]}`);
   } else {
     for (const item in helpTopics) {
       if (item in helpTopics) {
-        console.log(`    ${item}\t\t${helpTopics[item]}`);
+        console.log(`\t--${item}\t${helpTopics[item]}`);
       }
     }
   }
 }
 
-let paths = [];
-let command = commands.detect;
-function preparePaths(initial) {
-  paths = [];
-  for (let i = initial; i < process.argv.length; i + 1) {
-    paths.push(process.argv[i]);
+function detect(pths) {
+  // const files = [];
+  // for (const p of pths) {
+  //   files.push(getDuplicatesSync(p));
+  // }
+  return getDuplicatesSync(pths[0]);
+  // return new Promise((resolve, reject) => {
+  //   const duplicates = [];
+  //   for (const path of pths) {
+  //     getDuplicates(path).then((files) => {
+  //       duplicates.push(files);
+  //     }).catch((error) => {
+  //       console.log(error);
+  //     });
+  //   }
+  // });
+}
+
+/**
+ * Process the parameters in command line
+ * @return {Void}
+ */
+function processParameters() {
+  if (process.argv.length > 2) {
+    // Get the command
+    if (process.argv[2].substr(0, 2) === '--') {
+      command = process.argv[2].substr(2);
+    } else {
+      // Get the first path
+      paths.push(process.argv[2]);
+    }
+    // Process the others paths
+    for (let i = 3; i < process.argv.length; i++) {
+      paths.push(process.argv[i]);
+    }
+  } else {
+    paths.push('.');
   }
 }
 
 // Check parameters
-if (typeof process.argv[3] === 'undefined') {
-  command = commands.detect;
-  paths = ['.'];
-} else if (process.argv[3] === commands.help) {
-  help();
-} else if (process.argv[3] in commands) {
-  command = commands[process.argv[3]];
-  preparePaths(4);
+processParameters();
+
+if (command in commands) {
+  if (command === 'help') {
+    help(paths[0]);
+  } else if (command === 'detect') {
+    detect(paths);
+  } else if (command === 'link') {
+    detect(paths);
+  } else if (command === 'delete') {
+    detect(paths);
+  }
 } else {
-  command = commands.detect;
-  preparePaths(3);
+  help();
 }
-
-console.log(paths, Array.isArray(paths));
-
-console.log(command);
+console.log(paths, command);
