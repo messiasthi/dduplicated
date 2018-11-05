4d3753b7bd4ec37b7b8fde4eeab627bf96f8d12f
messiasthi <messiasthi@gmail.com>
2018-11-05 00:47:34 -0200

    Update in outputs and fix spaces and names.
    Signed-off-by: messiasthi <messiasthi@gmail.com>

--- dduplicated/cli.py
+++ dduplicated/cli.py
@@ -4,7 +4,7 @@ from sys import argv
 
 from dduplicated import commands
 
-def getPaths(params):
+def get_paths(params):
 	paths = []
 	for param in params:
 		path = opath.join(getcwd(), param)
@@ -13,30 +13,31 @@ def getPaths(params):
 
 	return paths
 
+
 def main():
 	params = argv
-
+	processed_files = []
 	# Remove the command name
 	del params[0]
 
 	if len(params) == 0 or "help" in params:
 		commands.help()
 	elif "detect" in params:
-		duplicates = commands.detect(getPaths(params))
-		if len(duplicates) < 1:
-			print("No duplicates found")
-			print("Great! Bye!")
-			exit(0)
-
-		for (key, values) in duplicates.items():
-			print(key + " -> ")
-			for value in values:
-				print("\t\t\t\t\t" + value)
+		processed_files = commands.detect(get_paths(params))
 
 	elif "delete" in params:
-		commands.delete(commands.detect(getPaths(params)))
+		processed_files = commands.delete(commands.detect(get_paths(params)))
+		
 	elif "link" in params:
-		commands.link(commands.detect(getPaths(params)))
+		processed_files = commands.link(commands.detect(get_paths(params)))
+	
 	else:
 		commands.help()
+	
+	if len(processed_files) > 0:
+		print(processed_files)
+	else:
+		print("No duplicates found")
+		print("Great! Bye!")
+		
 	exit(0)
