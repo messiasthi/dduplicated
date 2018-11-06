727939269aef168513ad6d62913e20f0af95b4e6
Thiago Messias <messiasthi@users.noreply.github.com>
2018-11-05 22:10:55 -0200

    Add reference to md5 method.

--- dduplicated/hashs.py
+++ dduplicated/hashs.py
@@ -3,7 +3,7 @@ import os
 
 def get_hash(path):
 	return get_md5(path)
-
+# MD5 methods is based on second answer from: https://exceptionshub.com/get-md5-hash-of-big-files-in-python.html
 def get_md5(path):
 	hash_md5 = hashlib.md5()
 	if os.path.isfile(path):
